59fea5b3db5a1671af4253ee3b77732a892c26ed
chiffon <chiffonlang@icloud.com>
2016-06-01 18:15:22 +0800

    img

--- R/wordcloud2.R
+++ R/wordcloud2.R
@@ -37,22 +37,25 @@
 ##'
 ##'
 ##'
-##' wordcloud2(demoFreq,size = 1)
-##' wordcloud2(demoFreq,size = 2)
+##' wordcloud2(demoFreq)
+##' wordcloud2(demoFreq, size = 2)
 ##'
-##' wordcloud2(demoFreq,size = 1,shape='pentagon')
-##' wordcloud2(demoFreq,size = 1,shape='star')
+##' wordcloud2(demoFreq, size = 1,shape = 'pentagon')
+##' wordcloud2(demoFreq, size = 1,shape = 'star')
 ##'
-##' wordcloud2(demoFreq,size = 2,
+##' wordcloud2(demoFreq, size = 2,
 ##'            color = "random-light", backgroundColor = "grey")
 ##'
-##' wordcloud2(demoFreq,size = 2, minRotation = -pi/2, maxRotation = -pi/2)
-##' wordcloud2(demoFreq,size = 2, minRotation = -pi/6, maxRotation = -pi/6,
+##' wordcloud2(demoFreq, size = 2, minRotation = -pi/2, maxRotation = -pi/2)
+##' wordcloud2(demoFreq, size = 2, minRotation = -pi/6, maxRotation = -pi/6,
 ##'   rotateRatio = 1)
-##' wordcloud2(demoFreq,size = 2, minRotation = -pi/6, maxRotation = pi/6,
+##' wordcloud2(demoFreq, size = 2, minRotation = -pi/6, maxRotation = pi/6,
 ##'   rotateRatio = 0.9)
 ##'
-
+##' wordcloud2(demoFreqC, size = 2,
+##'            color = "random-light", backgroundColor = "grey")
+##' wordcloud2(demoFreqC, size = 2, minRotation = -pi/6, maxRotation = -pi/6,
+##'   rotateRatio = 1)
 
 
 #' @import htmlwidgets
