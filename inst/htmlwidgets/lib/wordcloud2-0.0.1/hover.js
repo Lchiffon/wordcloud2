942f4cc56fd1d4c51f93ccb2a78232c042eea74d
chiffon <langdawei@shu.edu.cn>
2016-06-04 10:25:43 +0800

    add css

--- inst/htmlwidgets/lib/wordcloud2-0.0.1/hover.js
+++ inst/htmlwidgets/lib/wordcloud2-0.0.1/hover.js
@@ -5,23 +5,23 @@ function newlabel(el){
   newDiv.id = 'wcLabel'
   newSpan.id = "wcSpan"
 
-  newDiv.style.position = 'absolute';
-    newDiv.style.border ='2px solid #fff';
-    newDiv.setAttribute("box-shadow",'0 0 4px 0 #008');
-    newDiv.style.padding ='2px';
-    newDiv.style.margin ='-4px 0 0 -4px';
-    newDiv.setAttribute("pointer-events",'none');
-
-
-    newSpan.style.position ='absolute';
-    newSpan.style.top= '100%';
-    newSpan.style.left= '0';
-    newSpan.style.background= 'white';
-    newSpan.style.color= '#333';
-    newSpan.setAttribute("margin-top", '6px');
-    newSpan.style.padding= '0 0.5em';
-    newSpan.setAttribute('border-radius', '0.5em');
-    newSpan.setAttribute('white-space', 'nowrap');
+  // newDiv.style.position = 'absolute';
+  //   newDiv.style.border ='2px solid #fff';
+  //   newDiv.setAttribute("box-shadow",'0 0 4px 0 #008');
+  //   newDiv.style.padding ='2px';
+  //   newDiv.style.margin ='-4px 0 0 -4px';
+  //   newDiv.setAttribute("pointer-events",'none');
+  //
+  //
+  //   newSpan.style.position ='absolute';
+  //   newSpan.style.top= '100%';
+  //   newSpan.style.left= '0';
+  //   newSpan.style.background= 'white';
+  //   newSpan.style.color= '#333';
+  //   newSpan.setAttribute("margin-top", '6px');
+  //   newSpan.style.padding= '0 0.5em';
+  //   newSpan.setAttribute('border-radius', '0.5em');
+  //   newSpan.setAttribute('white-space', 'nowrap');
 
 
   el.appendChild(newDiv);
@@ -49,5 +49,5 @@ function cv_handleHover(item,
   document.getElementById("wcSpan").setAttribute(
     'data-l10n-args', JSON.stringify({ word: item[0], count: item[1] }));
   document.getElementById("wcSpan").innerHTML =item[0]+":" + item[1];
-  __(document.getElementById("wcSpan"));
+  //__(document.getElementById("wcSpan"));
 };
