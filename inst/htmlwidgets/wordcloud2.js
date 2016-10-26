caedfb0d21cfee36426de6ee6fcb58b48216069d
Lchiffon <chiffonlang@icloud.com>
2016-10-26 16:30:15 +0800

    Revert "use new `htmlwidget` js template and implement `resize`; see #6"

--- inst/htmlwidgets/wordcloud2.js
+++ inst/htmlwidgets/wordcloud2.js
@@ -1,76 +1,51 @@
 HTMLWidgets.widget({
-
-  name: 'wordcloud2',
-
-  type: 'output',
-
-  factory: function(el, width, height) {
-
-    var instance = {};
-
+  name: "wordcloud2",
+  type: "output",
+  initialize: function(el, width, height) {
     var newCanvas = document.createElement("canvas");
     newCanvas.height = height;
     newCanvas.width = width;
-    newCanvas.id = el.id + "-canvas";
+    newCanvas.id = "canvas";
 
     el.appendChild(newCanvas);
     newlabel(el);
-
-    var draw_wordcloud2 = function(instance){
-      var x = instance.x;
-
-      if(x.figBase64){
-        maskInit(el,x);
-      }else{
-        instance.wordcloud = WordCloud(el.firstChild, { list: listData,
-                        fontFamily: x.fontFamily,
-                        fontWeight: x.fontWeight,
-                        color: x.color,
-                        minSize: x.minSize,
-                        weightFactor: x.weightFactor,
-                        backgroundColor: x.backgroundColor,
-                        gridSize: x.gridSize,
-                        minRotation: x.minRotation,
-                        maxRotation: x.maxRotation,
-                        shuffle: x.shuffle,
-                        shape: x.shape,
-                        rotateRatio: x.rotateRatio,
-                        ellipticity: x.ellipticity,
-                        drawMask: x.drawMask,
-                        maskColor: x.maskColor,
-                        maskGapWidth: x.maskGapWidth,
-                        hover: x.hover || cv_handleHover
-                        });
-      }
+    return(el.firstChild);
+  },
+  renderValue: function(el, x, instance) {
+    // parse gexf data
+    listData=[];
+    for(var i=0; i<x.word.length; i++){
+      listData.push([x.word[i], x.freq[i]]);
+    }
+    if(x.figBase64){
+      maskInit(el,x);
+    }else{
+      WordCloud(el.firstChild, { list: listData,
+                      fontFamily: x.fontFamily,
+                      fontWeight: x.fontWeight,
+                      color: x.color,
+                      minSize: x.minSize,
+                      weightFactor: x.weightFactor,
+                      backgroundColor: x.backgroundColor,
+                      gridSize: x.gridSize,
+                      minRotation: x.minRotation,
+                      maxRotation: x.maxRotation,
+                      shuffle: x.shuffle,
+                      shape: x.shape,
+                      rotateRatio: x.rotateRatio,
+                      ellipticity: x.ellipticity,
+                      drawMask: x.drawMask,
+                      maskColor: x.maskColor,
+                      maskGapWidth: x.maskGapWidth,
+                      hover: x.hover || cv_handleHover
+                      });
     }
 
-    return {
+  },
 
-      renderValue: function(x) {
-        instance.x = x;
-
-        // parse gexf data
-        listData=[];
-        for(var i=0; i<x.word.length; i++){
-          listData.push([x.word[i], x.freq[i]]);
-        }
-        instance.listData = listData;
-
-        draw_wordcloud2(instance);
-
-      },
-
-      resize: function(width, height) {
-
-        newCanvas.height = height;
-        newCanvas.width = width;
-
-        draw_wordcloud2(instance);
-
-      },
-
-      instance: instance
-
-    };
+  resize: function(el, width, height) {
   }
+
+
+
 });
